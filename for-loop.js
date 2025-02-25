02a254a5e446a45964890835f485576248aac8a7
tektechnologies <info@tektechnologies.com>
2025-02-25 11:13:50 -0600

    add index file and js log proof of life

---
+++ for-loop.js
@@ -0,0 +1,17 @@
+console.log('for-loop.js');
+
+
+
+let romanToInt = function(stringOfNumbers){
+    let romanNumToInteger ={
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100, 
+        D: 500, 
+        M: 1000
+    }
+    console.log('Roman to Number: ', romanNumToInteger);
+
+}
